de72571cd436a6481fa5d1565e7ae374b226026c
IgorMaks <orkmedv@gmail.com>
2024-07-24 18:39:09 +0300

    Add files via upload

---
+++ src/test/java/config/ConfigUser.java
@@ -0,0 +1,16 @@
+package config;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static config.Config.*;
+import static io.restassured.RestAssured.*;
+
+public class ConfigUser {
+    public static RequestSpecification spec() {
+        return given()
+                .baseUri(BASE_URI)
+                .basePath(API_AUTH)
+                .contentType(ContentType.JSON);
+    }
+}
